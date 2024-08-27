6af3479f11084e406a1baec023b8bedae3953e1a
Beacita <00001095179986sp@al.educacao.sp.gov.br>
2024-08-27 11:43:35 -0300

    criando sites

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let posicaoXbolinha
+let posicaiYbolinha
+let diametro=20
+let cor
+
+function setup() {
+  createCanvas(600, 600);
+  background(240, 128, 128);
+  cor=color (random(0, 497), random(0, 497), random (0, 497))
+  
+}
+function draw(){
+  fill(cor)
+  circle(posicaoXbolinha, posicaoYbolinha, 20)
+  posicaoXbolinha=mouseX
+  posicaoYbolinha=mouseY
+  if (mouseIsPressed){
+  cor=color (random(0, 497), random(0, 497)random(0, 497))
+  
+  }
+}
+funcion mousePressed (){
+  background(240, 128, 128)
+  
+    
+}
